06f4fea12cbb5c04b006ff88dc5577e4709da0ad
Tim Menzies <tim.menzies@gmail.com>
2019-07-22 23:12:11 -0400

    saving

---
+++ src/minilib.py
@@ -0,0 +1,84 @@
+import sys,re,traceback,random
+
+r=random.random
+seed=random.seed
+one=random.choice
+
+def close(x,y,near=0.01): return y*(1-near) <=x<= y*(1+near)
+
+class o:
+  def __init__(i,**kw): i.__dict__.update(kw)
+
+def interpolate(x, xy):
+  x1, y1 = xy[0]
+  if x < x1: return y1
+  for x2, y2 in xy[1:]:
+    if x1 <= x < x2:
+      return y1 + (y2 - y1) * (x - x1) / (x2 - x1)
+    x1, y1 = x2, y2
+  return y2
+
+class Thing(object):
+  def __repr__(i):
+    pairs = sorted([(k,v) for k,v in i.__dict__.items()
+                    if k[0] != "-"])
+    pre = i.__class__.__name__ + '{'
+    q=lambda z: "'%s'" % z if isinstance(z,str)  else str(z)
+    return pre + ", ".join(['%s=%s' % (k,q(v)) 
+                          for k,v in pairs]) + '}'
+
+from functools import wraps
+
+def memo0(f):
+  "Memo index does NOT includes first argument"
+  name = f.__name__
+  @wraps(f)
+  def g(i, *arg, **kw):
+   if name not in i._memo:
+     i._memo[name] = f(i, *arg, **kw)
+   return i._memo[name]
+  return g
+
+def memos(k, what='0', decorator=memo0):
+  "Turn  methods ending with 'what' into a memoed property"
+  for f in dir(k):
+    if callable(getattr(k, f)):
+      if f[-1] == what:
+        setattr(k,f[:-1], property(decorator(getattr(k,f))))
+  return k
+
+
+def shuffle(lst):
+  random.shuffle(lst)
+  return lst
+
+class ok:
+  tries,fails = 0,0  #  tracks the record so far
+  d={}
+  def __init__(i,fun):
+    k=i.__class__.__name__
+    ok.d[k] = ok.d.get(k,[]) + [fun]
+  def run1(fun,k):
+    def score(t, f): 
+      return f"PASS= {t-f} FAIL= {f} %PASS = {(t-f)/(t+0.0001):.0%}"
+    try:
+      ok.tries += 1
+      if fun.__doc__:
+        print("# "+ re.sub(r'\n[ \t]*',"\n# ", fun.__doc__))
+      fun()
+    except Exception:
+      ok.fails += 1
+      print(traceback.format_exc())
+    print("##",k,score(ok.tries, ok.fails),':',fun.__name__)
+  def run(k=None):
+    if k:
+      for fun in ok.d.get(k,[]): ok.run1(fun,k)
+    else:
+      for k,funs in ok.d.items(): ok.run(k) 
+  def main(lst):
+    if len(lst)> 1 and lst[1] in ok.d:
+      ok.run(lst[1])
+    else:
+      ok.run()
+    if ok.fails>0: sys.exit(1)
+ 
---
+++ src/minitest.py
@@ -0,0 +1,14 @@
+import click
+
+@click.group()
+@click.option('--debug/--no-debug', default=False)
+def cli(debug):
+    click.echo('Debug mode is %s' % ('on' if debug else 'off'))
+
+@cli.command()
+def sync():
+   click.echo('Synching')
+
+
+if __name__ == '__main__':
+  cli()
